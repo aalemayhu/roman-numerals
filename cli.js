9e87b49cb105eb032aeafa08984254cc4f4f80a4
Alexander Alemayhu <alexander@alemayhu.com>
2019-01-20 14:04:31 +0100

    handle limits
    Signed-off-by: Alexander Alemayhu <alexander@alemayhu.com>

--- cli.js
+++ cli.js
@@ -6,14 +6,14 @@ const chalk = require("chalk");
 
 const conversion = require("./lib/conversion");
 
-cli.usage("arabic-number");
+cli.usage("arabic-number-greater-than-0-max-3999");
 cli.version(version);
 cli.help();
 
 const parsed = cli.parse();
 
 const number = parsed.args[0];
-if (number) {
+if (number >= 1 && number <= 3999) {
   console.log(number, "->", conversion.toRomanNumeral(number));
 } else {
   cli.outputHelp();
